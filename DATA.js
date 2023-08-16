8c83e33a2d9b3b71a1caabddddb500c04da75463
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-08-16 11:58:08 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "04372a92-58e3-4aa0-9f46-d2b15483b0da",
+    "home_feed": [
+        {
+            "id": "b92f17ce-8bca-4c0c-9412-06ffe00ef43c",
+            "artist_name": "stand",
+            "home_carousel": [
+                {
+                    "id": "8704b49e-a644-495f-a217-27d5f50357b4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/249.png"
+                },
+                {
+                    "id": "e06b54ac-d686-420c-adf1-8cd2f72a1b57",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/222.png"
+                },
+                {
+                    "id": "13460dad-3bcd-43fe-ae93-16c3c6d1620c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/216.png"
+                },
+                {
+                    "id": "2dbe7bff-8ba1-4f76-adbe-371bf3ad8c0e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/33.png"
+                },
+                {
+                    "id": "a8656198-176a-4097-a5ba-bb76acb7f417",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/221.png"
+                }
+            ],
+            "art_caption": "skill",
+            "art_description": "Animal voice result run front seek. Minute thought nothing region Congress anyone. Ready benefit civil big. Effect two black unit like."
+        },
+        {
+            "id": "9db23118-bb02-4975-a72d-386ee18d53bd",
+            "artist_name": "garden",
+            "home_carousel": [
+                {
+                    "id": "453ed61c-cbb1-4d02-b745-cb38ef030b32",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/316.png"
+                },
+                {
+                    "id": "b8c3888c-b711-4676-977a-f7b82f696c89",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/298.png"
+                },
+                {
+                    "id": "2cd27610-6492-4652-8ceb-0459f2335e56",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/215.png"
+                },
+                {
+                    "id": "95940e8a-1d79-4300-89cd-d1f6079851b9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/304.png"
+                },
+                {
+                    "id": "22d5d50b-ecf7-4fc0-b1c8-3b6087f8e4f4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/5.png"
+                }
+            ],
+            "art_caption": "parent",
+            "art_description": "Involve TV north standard friend there. Floor nature water risk movement. Customer author network those school bar."
+        },
+        {
+            "id": "70614210-46e4-413f-ae83-b899ac1ce1d8",
+            "artist_name": "something",
+            "home_carousel": [
+                {
+                    "id": "a62a76eb-f01f-4474-a31a-9d1a46a78545",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/193.png"
+                },
+                {
+                    "id": "d0e3ea42-c775-4176-bcba-98e16007a570",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png"
+                },
+                {
+                    "id": "a62cc218-01d7-472e-917e-a36a94542b5c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/107.png"
+                },
+                {
+                    "id": "5da03243-4977-49d7-8e50-59e2252c6544",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/253.png"
+                },
+                {
+                    "id": "237fc0f4-887a-4871-8b63-984536992060",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/181.png"
+                }
+            ],
+            "art_caption": "skill",
+            "art_description": "Dream decide program science small get. Onto sing view their black."
+        },
+        {
+            "id": "e93ada7c-c7af-4b34-8f10-d10b7fd0585b",
+            "artist_name": "soldier",
+            "home_carousel": [
+                {
+                    "id": "e18e643f-fc46-4cf0-931f-9caf318491a2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/138.png"
+                },
+                {
+                    "id": "345a2979-2131-4431-9067-69b7de8c8919",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/222.png"
+                },
+                {
+                    "id": "27cedac6-1d94-4b71-8441-7b2718ace233",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/90.png"
+                },
+                {
+                    "id": "38d56e92-5f66-4ae4-9d75-9898cc4e28fd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/179.png"
+                },
+                {
+                    "id": "995c4e68-6e9f-4dc6-bdd6-45494c8d2aaa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/3.png"
+                }
+            ],
+            "art_caption": "direction",
+            "art_description": "Without scientist expect know. Middle him meeting seem garden degree event size."
+        },
+        {
+            "id": "b380ec6b-3efe-4511-93e8-89c067ced0ad",
+            "artist_name": "that",
+            "home_carousel": [
+                {
+                    "id": "93f1132c-3644-46cc-a3dc-8a563927dc2e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/23.png"
+                },
+                {
+                    "id": "1246aa07-2d60-4d94-b5c6-f05f300dff0e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/235.png"
+                },
+                {
+                    "id": "f48bae28-e58a-4c82-9473-109195e58626",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/332.png"
+                },
+                {
+                    "id": "2be549d6-4436-4e53-b16c-ccb91238dfdc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/276.png"
+                },
+                {
+                    "id": "079cf485-6854-41b9-a2b5-eef4c25731cf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/299.png"
+                }
+            ],
+            "art_caption": "my",
+            "art_description": "Drop his investment provide successful."
+        }
+    ]
+}
